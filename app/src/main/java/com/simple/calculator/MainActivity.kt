960ec7b5a96ad9ada6913ade83bf567d475cac3b
otsimi <brightonobadia058@gmail.com>
2024-05-30 15:48:26 +0300

    guildline constrictions

--- app/src/main/java/com/simple/calculator/MainActivity.kt
+++ app/src/main/java/com/simple/calculator/MainActivity.kt
@@ -11,31 +11,42 @@ class MainActivity : AppCompatActivity() {
     private var operator = ""
     private var firstOperand = 0.0
     private var secondOperand = 0.0
-    private lateinit var display: TextView
+    private lateinit var viewHolder:calculatorViewHolder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        viewHolder = calculatorViewHolder(findViewById(R.id.main))
 
-        display = findViewById(R.id.textview)
+        if (savedInstanceState != null) {
+            currentInput = savedInstanceState.getString("currentInput", "")
+            operator = savedInstanceState.getString("operator", "")
+            firstOperand = savedInstanceState.getDouble("firstOperand", 0.0)
+            secondOperand = savedInstanceState.getDouble("secondOperand", 0.0)
+            viewHolder.display.text = savedInstanceState.getString("displayText", "0")
+        }
+
         setNumberButtonListeners()
         setOperatorListeners()
-        findViewById<Button>(R.id.buttonClear).setOnClickListener { clear() }
-        findViewById<Button>(R.id.buttonEquals).setOnClickListener { calculateResult() }
+        viewHolder.buttonClear.setOnClickListener { clear() }
+        viewHolder.buttonEquals.setOnClickListener { calculateResult() }
     }
 
     private fun setNumberButtonListeners() {
         val buttons = listOf(
-            findViewById<Button>(R.id.button0),
-            findViewById<Button>(R.id.button1),
-            findViewById<Button>(R.id.button2),
-            findViewById<Button>(R.id.button3),
-            findViewById<Button>(R.id.button4),
-            findViewById<Button>(R.id.button5),
-            findViewById<Button>(R.id.button6),
-            findViewById<Button>(R.id.button7),
-            findViewById<Button>(R.id.button8),
-            findViewById<Button>(R.id.button9)
+            viewHolder.button0,
+            viewHolder.button1,
+            viewHolder.button2,
+            viewHolder.button3,
+            viewHolder.button4,
+            viewHolder.button5,
+            viewHolder.button6,
+            viewHolder.button7,
+            viewHolder.button8,
+            viewHolder.button9,
+            viewHolder.buttonClear,
+            viewHolder.buttonEquals
+
         )
         for (button in buttons) {
             button.setOnClickListener { appendNumber(button.text.toString()) }
@@ -44,10 +55,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setOperatorListeners() {
         val operators = mapOf(
-            findViewById<Button>(R.id.buttonplus) to "+",
-            findViewById<Button>(R.id.buttonminus) to "-",
-            findViewById<Button>(R.id.buttonmultiply) to "*",
-            findViewById<Button>(R.id.buttondivide) to "/"
+            viewHolder.buttonPlus to "+",
+            viewHolder.buttonMinus to "-",
+            viewHolder.buttonMultiply to "*",
+            viewHolder.buttonDivide to "/"
         )
         for ((button, op) in operators) {
             button.setOnClickListener { setOperator(op) }
@@ -56,14 +67,14 @@ class MainActivity : AppCompatActivity() {
 
     private fun appendNumber(number: String) {
         currentInput += number
-        display.text = currentInput
+        viewHolder.display.text = currentInput
     }
 
     private fun setOperator(op: String) {
         if (currentInput.isNotEmpty()) {
             firstOperand = currentInput.toDouble()
             currentInput += " $op "
-            display.text = currentInput
+            viewHolder.display.text = currentInput
             currentInput = ""
             operator = op
         }
@@ -79,7 +90,7 @@ class MainActivity : AppCompatActivity() {
                 "/" -> firstOperand / secondOperand
                 else -> 0.0
             }
-            display.text = "${firstOperand.toString()} $operator ${secondOperand.toString()} = $result"
+            viewHolder.display.text = "${firstOperand.toString()} $operator ${secondOperand.toString()} = $result"
             currentInput = result.toString()
         }
     }
@@ -89,6 +100,14 @@ class MainActivity : AppCompatActivity() {
         operator = ""
         firstOperand = 0.0
         secondOperand = 0.0
-        display.text = "0"
+        viewHolder.display.text = "0"
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString("currentInput", currentInput)
+        outState.putString("operator", operator)
+        outState.putDouble("firstOperand", firstOperand)
+        outState.putDouble("secondOperand", secondOperand)
+        outState.putString("displayText", viewHolder.display.text.toString())
     }
 }
